db03dbffcf503cb1a039ae72f3847585f5702588
mdshehabsk <mdshehabsk@gmail.com>
2026-08-05 00:26:02 +0600

    Refactor code structure for improved readability and maintainability

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,6 +5,7 @@ import Hero from "@/components/Hero";
 import Navbar from "@/components/Navbar";
 import StatsBanner from "@/components/StatsBanner";
 import  Footer  from "@/components/Footer";
+import Image from "next/image";
 
 const New = () => {
   return (
@@ -12,6 +13,7 @@ const New = () => {
     <Navbar />
     <Hero/>
     <Features/>
+    <Image src='/another.jpeg' alt="logo iamge" width={1000} height={200} className="w-full   " />
     <Founder/>
     <Portfolio/>
     <StatsBanner/>
